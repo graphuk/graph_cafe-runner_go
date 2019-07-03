fead62936bef3c4022d1f4856e90797d79c6dd30
alexander.eliseev <alexander.eliseev@graph.uk>
2019-07-03 17:21:18 +0500

    Extended debug output

--- scripts/buildInJenkins.stage3.cmd
+++ scripts/buildInJenkins.stage3.cmd
@@ -1,6 +1,9 @@
 k:
 set PATH=%PATH%;%CD%/node_modules/hub-win
 set
+echo %1
+echo %2
 set GITHUB_TOKEN=%2
 set
+
 hub release create -t master -m testTitle -a release\cafe-runner-server.exe -a release\cafe-runner-server %1
