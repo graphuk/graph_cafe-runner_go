24f61b2456052056cdbbaa429b60227c3a50aea9
alexander.eliseev <alexander.eliseev@graph.uk>
2019-09-05 12:27:33 +0500

    Update model: show time ago instead of timestamp

--- web/testpacks/models/testpacks-list-model.go
+++ web/testpacks/models/testpacks-list-model.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"strings"
 	"time"
 
 	"github.com/graph-uk/graph_cafe-runner_go/data/repositories"
@@ -9,19 +10,30 @@ import (
 )
 
 type testpackRec struct {
-	ID         int
-	UploadTime time.Time
+	ID      int
+	TimeAgo string
 }
 
 type testpacksListModel struct {
 	Tesptacks []testpackRec
 }
 
+//return duration like 14h27m5s
+func timeAgoHumanString(now, moment time.Time) string {
+	res := now.Sub(moment).String() //14h27m5.4421878s
+	dotIndex := strings.Index(res, `.`)
+	if dotIndex != -1 {
+		res = res[:dotIndex] + `s` //14h27m5s
+	}
+	return res
+}
+
 func NewTestpacksListModel(DB *storm.DB) *testpacksListModel {
 	res := &testpacksListModel{}
+	now := time.Now()
 	allTestpacks := (&repositories.Testpacks{DB}).FindAll()
 	for _, curTestpack := range *allTestpacks {
-		res.Tesptacks = append(res.Tesptacks, testpackRec{curTestpack.ID, curTestpack.UploadTime})
+		res.Tesptacks = append(res.Tesptacks, testpackRec{curTestpack.ID, timeAgoHumanString(now, curTestpack.UploadTime)}) //curTestpack.UploadTime})
 	}
 	return res
 }
