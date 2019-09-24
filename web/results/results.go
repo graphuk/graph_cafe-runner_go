d4442e3e4c2c4f4d37e07959e4ddc071d7b39425
alexander.eliseev <alexander.eliseev@graph.uk>
2019-09-24 12:21:07 +0500

    Delete text from empty cells

--- web/results/results.go
+++ web/results/results.go
@@ -2,7 +2,6 @@ package results
 
 import (
 	"net/http"
-	//	"strconv"
 
 	"github.com/asdine/storm"
 	"github.com/graph-uk/graph_cafe-runner_go/web/results/models"
@@ -24,11 +23,3 @@ func (t *Handler) Results(c echo.Context) error {
 	model := models.NewResultsModel(t.DB)
 	return c.Render(http.StatusOK, `results/views/results.html`, model)
 }
-
-// func (t *Handler) Runtest(c echo.Context) error {
-// 	//testpackID, err := strconv.Atoi(c.Param(`id`))
-// 	check(err)
-
-// 	model := models.NewResultsModel(t.DB)
-// 	return c.Render(http.StatusOK, `results/views/testpack.html`, model)
-// }
